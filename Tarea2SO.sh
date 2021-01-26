b34df74e6ebb569fc6f8ab3f8ed6f5f6890c71ad
_glitch_404 <45013989+Rubencrash236@users.noreply.github.com>
2021-01-26 00:09:28 -0400

    Simple script for navigating and listing fragmentation

--- Tarea2SO.sh
+++ Tarea2SO.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 #author: Ruben Osmani Diaz Cabrera
-#purpose: Simple bash script 
+#purpose: Simple bash script that navigates to the bin directory first and list all the files 
+#         that have the letter of choice as the third letter of the file name.
 #           
-#            
-#           
+#         Then it gives liberty to navigate to any directory and list all the files and folders.
+#         If the entry of action is p it prints the ls output to the screen else if the entry for action
+#         is a it appends the ls output to a text file in the directory that the script is executed
+
+
 set -e #tells the script to terminate if any command exited with a nonzero exit status
 set -u #tells Bash scripts not to run any command containing a reference to an unset variable name
 #set -o pipefail #if the last program terminates with a nonzero status, the pipe will not be terminated
@@ -12,11 +16,11 @@ currentDirectory=`pwd`
 appendFiles="${currentDirectory}/appendedFiles.txt"
 
 cd /bin 
-echo "Current directory: " 
+echo -n "Current directory: " 
 pwd
 echo "\n"
 
-echo "Third letter of your name:" 
+echo -n "Third letter of the files: " 
 read letter
 
 ls ??$letter*
@@ -26,15 +30,20 @@ ls [aeiou]*
 echo "All files names dont start with a consonant\n\n\n"
 echo -n "Press any key to continue..."
 read cont 
-clear 
 
+clear 
+#///////////////////////// Second Part of Script \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 cd ~
+echo -n "Current Directory: "
+pwd
+echo "\n"
+
 echo -n "Directory path: "
 read path
 
 cd $path
 
-echo "Action to make p(print to screen) or a(append to a file): "
+echo -n "Action to make p(print to screen) or a(append to a file): "
 read action 
 
 if [ "$action" = "a" ]; then
