d7f6e33d0e4146f90355b0de03d029366ccac613
_glitch_404 <45013989+Rubencrash236@users.noreply.github.com>
2021-02-05 20:05:24 -0400

    Comentaries changed

--- Tarea3SO/punto4.sh
+++ Tarea3SO/punto4.sh
@@ -2,7 +2,7 @@
 #author: Ruben Osmani Diaz Cabrera
 #purpose: This Script uses the directory tree created in punto2.sh
 #         This script makes a backup of of the directory tree 
-#               in the directory.bak file located inthe Local directory
+#               in the copiaSeguridad.bak file located inthe Local directory
 
 cd Mail
 find . -type f -exec cat {} >> Local/copiaSeguridad.bak +;
